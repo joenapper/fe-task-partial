cad6547a4c544391f38804b108d5179f34c2d1ce
joenapper <joe.napper@hotmail.co.uk>
2021-09-05 22:56:20 +0100

    step 1

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+const Card = () => {
+  return (
+    <div className="movie">
+      <h2 className="movie-title">Wonder Woman 1984</h2>
+      <p>
+        Wonder Woman comes into conflict with the Soviet Union during the Cold
+        War in the 1980s and finds a formidable foe by the name of the Cheetah.
+      </p>
+      <p>Rating: 7/10</p>
+      <p>Popularity: 2407.318</p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Genre.js
@@ -0,0 +1,9 @@
+const Genre = () => {
+  return (
+    <div>
+      <h1>Genres</h1>
+    </div>
+  );
+};
+
+export default Genre;
